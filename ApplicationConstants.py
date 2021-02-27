a0f1c6fd8e0dd15ccadd421a547d966fa6ba86ae
Oleh Kopylchak <38405168+0lezhka@users.noreply.github.com>
2021-02-27 18:18:15 +0200

    CW-2 Add Logging functionality. (#3)

--- ApplicationConstants.py
+++ ApplicationConstants.py
@@ -1,5 +1,6 @@
 CSV_FILE_SEPARATOR = ";"
 CONFIGS_FILE_PATH = "resources/importConfig.json"
+LOG_FILE_PATH = "logs/applicationLogs.log"
 DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 DATE_TIME_REGEX = "(?P<transactionYear>\\d{4})-(?P<transactionMonth>\\d{2})-(?P<transactionDay>\\d{2}) " \
                   "(?P<transactionHour>\\d{2}):(?P<transactionMinute>\\d{2}):(?P<transactionSecond>\\d{2})"
