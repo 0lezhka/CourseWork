724b381a98f8eb7c46005bef1be6597abcd7fb01
Oleh Kopylchak <38405168+0lezhka@users.noreply.github.com>
2021-02-27 18:11:11 +0200

    CW-1 Add process_data command that exclude columns from config file and convert datetime column to atomic separate columns. (#1)

---
+++ ApplicationConstants.py
@@ -0,0 +1,8 @@
+CSV_FILE_SEPARATOR = ";"
+CONFIGS_FILE_PATH = "resources/importConfig.json"
+DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+DATE_TIME_REGEX = "(?P<transactionYear>\\d{4})-(?P<transactionMonth>\\d{2})-(?P<transactionDay>\\d{2}) " \
+                  "(?P<transactionHour>\\d{2}):(?P<transactionMinute>\\d{2}):(?P<transactionSecond>\\d{2})"
+ORDERS_FILE_TYPE = "orders"
+INFOS_FILE_TYPE = "infos"
+ITEMS_FILE_TYPE = "items"
